7f0897f75e4a24d1cfc81c662c16779975b2a6b0
Jesse Eikema <jesse@voorhoede.nl>
2019-05-07 17:25:23 +0200

    update model

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const project = 'mentos-ml-exp-235009';
 const region = 'us-central1';
-const automl_model = 'ICN8080937224687214689';
+const automl_model = 'ICN5755968781999654599';
 const automl = require('@google-cloud/automl');
 const predictionClient = new automl.PredictionServiceClient();
 // Firebase libraries
